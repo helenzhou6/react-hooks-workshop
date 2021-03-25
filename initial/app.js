1c12be5f2c29704cc4355a8a3257fea1ce683c76
Helen Zhou <helen.zhou6@gmail.com>
2021-03-25 07:37:58 +0000

    Update task notes

--- initial/app.js
+++ initial/app.js
@@ -14,11 +14,18 @@ const fetchBookInfo = isbn =>
   );
 
 // ### Task
-// 1. Use fetchBookInfo() to fetch book data on page load
-// 2. Display the book title(s) or "Loading" when waiting for data
-// 4. Then fetch other books from same author - using the author name of the first book `data.items[0].volumeInfo.authors[0]`
-//     - Use fetchAuthorBooks()
-// 5. Display error messages if api call fails
+// Final outcome: https://raw.githubusercontent.com/helenzhou6/react-hooks-workshop/master/final.gif
+// 1. Use provided fetchBookInfo(isbn) to fetch the book data on component mount
+// 2. Under 'Your Book' heading
+//     - display "Loading" when waiting for the API call to finish
+//     - then display the book title(s)
+// 4. Then fetch other books from same author, once the original API call has finished.
+//     - Use the author from the first API call (`data.items[0].volumeInfo.authors[0]`)
+//     - And use provided `fetchAuthorBooks(author)`
+// 5. Under the 'Other Books from the same author', display "Loading" when waiting for the book
+//     - display "Loading" when waiting for second API call to finish
+//     - then display the other books by the same author
+// 5. Display an error message if either API call fails
 // 6. Refactor to use async / await and try / catch
 
 const App = () => {
